a82f719c65666794fc194ba9c10cafd5c38facf9
John Ballon <johnballon@Johns-MacBook-Pro.local>
2017-06-05 18:12:28 -0700

    Successfully render blank page

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { AppRegistry } from 'react-native';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+import { Text, View } from 'react-native';
 
-export default class frontend extends Component {
+export default class PlanIt extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+        <Text>We're in boys</Text>
+        <Text>We're in boys</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
-AppRegistry.registerComponent('frontend', () => frontend);
+
+AppRegistry.registerComponent('frontend', () => PlanIt);
