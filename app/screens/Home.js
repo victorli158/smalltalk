e8367fcabc3498266f048a1b51b6c607171625bc
Victor Li <victor.li158@gmail.com>
2017-06-06 11:27:30 -0700

    Move index.ios.js scripts to index.js, set up Home screen

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,4 +1,16 @@
 import React, { Component, PropTypes } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, View, Text } from 'react-native';
 
 import colors from '../config/colors';
+
+class Header extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Home</Text>
+      </View>
+    );
+  }
+}
+
+export default Header;
