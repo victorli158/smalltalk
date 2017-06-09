5522efc2b8a2aa23e07515bbc7b953e2859e32f8
mileshobby <mhobby16@gmail.com>
2017-06-08 22:07:36 -0700

    edit get on signup to print current user for testing purposes

--- backend/planit/settings.py
+++ backend/planit/settings.py
@@ -55,6 +55,7 @@ CHANNEL_LAYERS = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     )
 }
 
