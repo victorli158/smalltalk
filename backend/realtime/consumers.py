1c631db50f66ca4d5d7a85421a97a219afe42af3
mileshobby <mhobby16@gmail.com>
2017-06-09 11:11:44 -0700

    refactor ws_add to create new connection with two users upon web-socket initial receival, unless no open connections exist in which case create a new one

--- backend/realtime/consumers.py
+++ backend/realtime/consumers.py
@@ -1,5 +1,7 @@
 # In consumers.py
+import pdb
 from channels import Channel, Group
+from realtime.models import Connection
 from channels.sessions import channel_session
 from channels.auth import channel_session_user, channel_session_user_from_http
 
@@ -7,9 +9,22 @@ from channels.auth import channel_session_user, channel_session_user_from_http
 @channel_session_user_from_http
 def ws_add(message):
     # Accept the connection
+    #TODO ADD USER AUTHENTIXATION HERE
+    # if request.user.is_authenticated:
     message.reply_channel.send({"accept": True})
-    # Add to the chat group
-    Group("chat").add(message.reply_channel)
+
+    #get all chatrooms
+    connections = Connection.objects.all()
+    #see if there are any chatrooms that are open (only 1 user)
+    for connection in connections:
+        if connection.users.count():
+            connection.users.add(message.user)
+            Group(str(connection.pk)).add(message.reply_channel)
+            return
+
+    #if no open chats
+    connection = Connection()
+    connections.users.add(message.user)
 
 # Connected to websocket.receive
 @channel_session_user
@@ -21,4 +36,5 @@ def ws_message(message):
 # Connected to websocket.disconnect
 @channel_session_user
 def ws_disconnect(message):
+    #TODO MAKE SURE TO CHECK IF THE SIZE OF GROUP IS EMPTY, IF SO DELETE GROUP
     Group("chat").discard(message.reply_channel)
