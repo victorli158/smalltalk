9fd274ab94ab824b750c53677345982f68fe142d
mileshobby <mhobby16@gmail.com>
2017-06-17 14:16:38 -0700

    update changes

--- app/screens/Chat/ChatContainer.js
+++ app/screens/Chat/ChatContainer.js
@@ -1,11 +1,14 @@
 import { connect } from 'react-redux';
 import Chat from './Chat';
+import { getSessionKey } from '../../actions/session/sessionActions';
 
 const mapStateToProps = ({ session }) => ({
-  session
+  session,
+  session2: session
 });
 
 const mapDispatchToProps = (dispatch) => ({
+  getSessionKey: () => dispatch(getSessionKey()),
 
 });
 
